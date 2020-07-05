f54632012f38231c9e6fddcfb4a1634c5548c177
Kalle Lilja <15094562+ThatKalle@users.noreply.github.com>
2020-07-05 11:14:05 +0200

    Version 1.0.0

---
+++ ClassicAutoBWLOrb.lua
@@ -0,0 +1,24 @@
+-- File: ClassicAutoBWLOrb.lua
+-- Name: ClassicAutoBWLOrb
+-- Author: ThatKalle
+-- Description: Automatically enter Blackwing Lair when interacting with the Orb of Command.
+-- Version: 1.0.0
+
+local E = CreateFrame("Frame")
+E:RegisterEvent("GOSSIP_SHOW")
+E:SetScript("OnEvent", function(self, event, ...)
+    return self[event] and self[event](self, ...)
+end)
+
+function E:GOSSIP_SHOW()
+    local npcGuid = UnitGUID("npc") or nil
+    if npcGuid then
+        local void, void, void, void, void, npcID = strsplit("-", npcGuid)
+        if (not npcID) then return end
+        -- Orb of Command
+        if npcID == "179879" then
+            -- Orb of Command only has one gossipOption
+            SelectGossipOption(1)
+        end
+    end
+end
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ClassicAutoBWLOrb
+
+Upon interaction with the [Orb of Command](https://classic.wowhead.com/object=179879/orb-of-command) in Blackrock Spire the addon will automatically enter the [Blackwing Lair](https://classic.wowhead.com/blackwing-lair) raid instance. *If attuned and in a raid party*
+
+## Installation
+
+Make sure the folder is named `ClassicAutoBWLOrb` in your `Interface\AddOns` folder. If you download the master branch from GitHub it will add `-master` to the folder name.
